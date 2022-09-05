1e6d00c1c577eafaebeb59399cdc9f4e848eada7
wjdghks963 <chsw000@naver.com>
2022-09-05 15:43:52 +0900

    base setup

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express, { application } from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on 3000`);
+
+app.listen(3000, handleListen);
